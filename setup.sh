816701d1931ffdc24e8922c42615499d1e8aba21
darwinharianto <44696192+darwinharianto@users.noreply.github.com>
2019-03-25 12:06:27 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,28 +5,27 @@ sudo apt-get upgrade -y
 
 sudo apt install libnfc5 libnfc-bin libnfc-examples -y
 sudo apt-get remove gnome-terminal && sudo apt-get install gnome-terminal
-sudo pip install nfcpy -y
-
 sudo apt-get install python-pip
-sudo apt-get install libglib2.0-dev
+sudo apt-get  install  libusb-dev  libpcsclite-dev  i2c-tools  -y
+
+
+sudo pip install nfcpy
+sudo apt-get install libglib2.0-dev -y
 sudo pip install bluepy
 
 
-sudo apt-get update
-sudo apt-get  install  libusb-dev  libpcsclite-dev  i2c-tools  -y
-
-cd    ~
-wget   http://dl.bintray.com/nfc-tools/sources/libnfc-1.7.1.tar.bz2
+cd ~
+wget http://dl.bintray.com/nfc-tools/sources/libnfc-1.7.1.tar.bz2
 tar -xf libnfc-1.7.1.tar.bz2
 
-cd   libnfc-1.7.1
-./configure   --prefix=/usr   --sysconfdir=/etc
+cd libnfc-1.7.1
+./configure --prefix=/usr --sysconfdir=/etc
 make
-sudo   make   install
+sudo make install
 
-sudo   raspi-config
+sudo raspi-config
 
-sudo   mkdir   /etc/nfc
-sudo   nano   libnfc.conf
+sudo mkdir   /etc/nfc
+sudo nano   libnfc.conf
 
 sudo nano /home/pi/.config/lxsession/LXDE-pi/autostart
