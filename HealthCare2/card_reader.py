223011e7f0b8d605097808350bb7ccc780a79dd5
darwinharianto <44696192+darwinharianto@users.noreply.github.com>
2019-03-27 14:39:33 +0900

    Update card_reader.py

--- HealthCare2/card_reader.py
+++ HealthCare2/card_reader.py
@@ -4,6 +4,7 @@ import os
 import nfc
 import time
 import binascii
+import string
 
 
 # ----------------------------------------------------------------------- #
@@ -30,5 +31,19 @@ class NFC_byUSB(object):
 # ----------------------------------------------------------------------- #
 
 class CardReader_GPIO:
-    def empty(self):
-        return None
+    
+    def read_wait(self):
+        self.connect()
+        return True, self.idm
+        
+    
+    def on_connect(self):
+        self.proc = str(self.proc)
+        self.idm = str(str(proc.split(":")[4:5]).replace(" ", "").split("\\n")[0][2:])
+        if (not (all(c in string.hexdigits for c in idm)) || len(idm) != 24):
+            raise AttributeError("Not valid ID")
+        
+        
+    def connect(self):
+        self.proc = subprocess.check_output(["nfc-poll"])
+        on_connect();
