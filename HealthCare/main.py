36e1898607837943db7d07b4a86259915b113a80
darwinharianto <44696192+darwinharianto@users.noreply.github.com>
2019-03-27 17:47:42 +0900

    Update main.py

--- HealthCare/main.py
+++ HealthCare/main.py
@@ -7,15 +7,8 @@ import commands
 import buzzer
 from time import sleep
 
-#b = nfcctl.readNFC()
-#nfcctl.createUser(a)
-#nfcctl.logUser(a)
-#nfcctl.updateWeight("aa", 22)
-
-#a = nfcctl.exchangeBLE(b,"00000000-0000-0000-0000-000000000002")
-#print(a)
 buzzer.buzzer(4,2000,1,1)
-
+sleep(5)
 configDirectory = "/home/pi/HealthCare/HealthCare/config.txt"
 
 with open(configDirectory) as file:
@@ -41,36 +34,28 @@ while True:
     a = commands.getoutput('lsusb')
     if "Sony" in a:
         userID = nfcctl.readNFC()
-        print(userID)
-	if userID is None:
-	    continue
-        if config == "Entrance":
-            print("logging user")
-            nfcctl.logUser(userID)
-        elif config == "Application":
- 	    print("create new user")
-            nfcctl.createUser(userID)
-        elif config == "Body Weight":
-	    print("update user weight")
-            weight = nfcctl.exchangeBLE(userID, "00000000-0000-0000-0000-000000000002")
-            nfcctl.updateWeight(userID,weight)
-        else:
-            print("Error 404")
     else:
-        userID = nfcctl.readByPN532()
-        print(userID)
-	if userID is None:
-	    continue
-        if config == "Entrance":
-            print("logging user")
-            nfcctl.logUser(userID)
-        elif config == "Application":
- 	    print("create new user")
-            nfcctl.createUser(userID)
-        elif config == "Body Weight":
-	    print("update user weight")
-            weight = nfcctl.exchangeBLE(userID, "00000000-0000-0000-0000-000000000002")
-            nfcctl.updateWeight(userID,weight)
-        else:
-            print("Error 404")
-    
+        try:
+            userID = nfcctl.readByPN532()
+        except subprocess.CalledProcessError, e:
+            print("No data detected until timed out")
+            continue
+        
+    if userID is None:
+	continue
+    if config == "In Entrance":
+        print("logging user")
+        nfcctl.loginUser(userID)
+    elif config == "Out Entrance":
+ 	print("create new user")
+        nfcctl.logoutUser(userID)
+    elif config == "Application":
+ 	print("create new user")
+        nfcctl.createUser(userID)
+    elif config == "Body Weight":
+	print("update user weight")
+        weight = nfcctl.exchangeBLE(userID, "00000000-0000-0000-0000-000000000002")
+        nfcctl.updateWeight(userID,weight)
+    else:
+        print("Error 404")
+
